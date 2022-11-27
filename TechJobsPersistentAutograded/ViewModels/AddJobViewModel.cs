afaa819733f6b73349f0f1ef3b09583dc33e22a1
codeswitchingstl <codeswitching@icloud.com>
2022-11-27 11:46:46 -0600

    part 2 update added AddJobViewModel

---
+++ TechJobsPersistentAutograded/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistentAutograded.Models;
+
+namespace TechJobsPersistentAutograded.ViewModels
+{
+    public class AddJobViewModel
+    {
+        [Required(ErrorMessage = "Job name is required")]
+        public string Name { get; set; }
+        public int EmployerId { get; set; }
+        public List<SelectListItem> Employers { get; set; }
+
+        public AddJobViewModel()
+        {
+        }
+    }
+}
+
