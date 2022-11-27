a398b2fb051310ba425bd3454faf80d7881ae29a
codeswitchingstl <codeswitching@icloud.com>
2022-11-27 08:51:58 -0600

    added AddEmployerViewModel

---
+++ TechJobsPersistentAutograded/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistentAutograded.Models;
+
+namespace TechJobsPersistentAutograded.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Employer name is required")]
+        public string EmployerName { get; set; }
+
+        [Required(ErrorMessage = "EMployer location is required")]
+        public string EmployerLocation { get; set; }
+
+        /*public AddEmployerViewModel(Job theJob, List<Skill> possibleSkills)
+        {
+            Skills = new List<SelectListItem>();
+
+            foreach (var skill in possibleSkills)
+            {
+                Skills.Add(new SelectListItem
+                {
+                    Value = skill.Id.ToString(),
+                    Text = skill.Name
+                });
+            }
+
+            Job = theJob;
+        }*/
+
+        public AddEmployerViewModel()
+        {
+        }
+    }
+}
+
